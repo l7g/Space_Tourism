5d7cd18a114edd46f875cde9858622d041886c42
l7g <laurent.gagne@icloud.com>
2022-08-19 23:41:10 +0200

    feat: destination page copleta

--- script/script.js
+++ script/script.js
@@ -1,34 +1,58 @@
-//Impostazione sfondo home page
-const homeContainer = document.querySelector('.home-container');
-let initialBgImg = '../starter-code/assets/home/background-home-desktop.jpg';
-let url = initialBgImg;
+//Impostazione sfondi
+const homeContainer = document.querySelector('body');
+const initialBgImg = '../starter-code/assets/home/background-home-desktop.jpg';
+const destinatonBgImg = '../starter-code/assets/destination/background-destination-desktop.jpg'
 function setHomeBg(){
+    let url = initialBgImg;
+    homeContainer.style.backgroundImage = `url(${url})`;
+}
+function setDestinationBg(){
+    let url = destinatonBgImg;
     homeContainer.style.backgroundImage = `url(${url})`;
 }
 window.addEventListener('DOMContentLoaded', setHomeBg);
 
 //Event listener per selezione navbar
 let currentStep = document.querySelector('#s00');
-const homePage = document.querySelector('.main');
 currentStep.style.borderBottom = "2px solid white";
-console.log(currentStep.id)
+//definizione pagine
+const homePage = document.querySelector('.main');
+const destinationPage = document.querySelector('.destination');
+//setting homepage come pagine visibile al lancio
+destinationPage.style.display = 'none';
+//Funzione borders
 function stepsBorder(id) {
     let prevStep = currentStep;
     currentStep = document.querySelector(`#${id}`);
     currentStep.style.borderBottom = "2px solid white";
     prevStep.style.border = 'none';
-    if (currentStep != document.querySelector('#s00')) {
-        homePage.style.display = 'none';
-    } else {
+}
+//Funzione selettore sezione
+function stepsSelector(id){
+    currentStep = document.querySelector(`#${id}`);
+    if (currentStep == document.querySelector('#s00')) {
         homePage.style.display = 'flex';
+        setHomeBg();
+    } else {
+        homePage.style.display = 'none';
+    }
+    if (currentStep == document.querySelector('#s01')) {
+        destinationPage.style.display = 'flex';
+        setDestinationBg();
+    } else {
+        destinationPage.style.display = 'none';
     }
 }
+
 const steps = document.querySelectorAll('.step');
 steps.forEach(step => step.addEventListener('click', (e) => {
     stepsBorder(e.target.id);
+    stepsSelector(e.target.id);
 }))
 
-//Event listeners per hover selezione navbar
+
+
+//Event listeners per hover selezione navbar e selettore pianeti
 //mouseover
 function mouseoverEffect(id){
     let currStep = document.querySelector(`#${id}`);
@@ -40,6 +64,7 @@ function mouseoverEffect(id){
 steps.forEach(step => step.addEventListener('mouseover', (e) => {
     mouseoverEffect(e.target.id)
 }))
+
 //mouseout
 function mouseOutEffect(id){
     let currStep = document.querySelector(`#${id}`);
@@ -47,9 +72,22 @@ function mouseOutEffect(id){
         currStep.style.borderBottom = "none";
     }
 }
+
 steps.forEach(step => step.addEventListener('mouseout', (e) => {
     mouseOutEffect(e.target.id)
 }))
+
+//Event listener per bottone 'Esplora'
+const exploreButton = document.querySelector('.main-button');
+
+function exploreDestinations(){
+    homePage.style.display = 'none';
+    destinationPage.style.display = 'flex';
+    setDestinationBg();
+    stepsBorder('s01')
+}
+
+exploreButton.addEventListener('click', exploreDestinations)
 
 
 
